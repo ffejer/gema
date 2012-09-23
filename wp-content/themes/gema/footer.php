f0b39cff505ae1f65bc1bc8e08abfd3001c2a873
Ferenc Fejer <FerencFejer@gmail.com>
2012-09-23 22:16:42 +0200

    elastic slider to rubriken page.

--- wp-content/themes/gema/footer.php
+++ wp-content/themes/gema/footer.php
@@ -33,8 +33,8 @@
 </body>
 
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery-1.8.1.min.js" type="text/javascript"></script>
-<?php global $elasticEnabled;Ê?>
-<?php if($elasticEnabled): ?>
+<?php global $elasticSliderEnabled;Ê?>
+<?php if($elasticSliderEnabled): ?>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.easing.1.3.js" type="text/javascript"></script>
 <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.eislideshow.js" type="text/javascript"></script>
 <script type="text/javascript">
--- wp-content/themes/gema/page-rubriken.php
+++ wp-content/themes/gema/page-rubriken.php
@@ -12,6 +12,9 @@
  * @since Twenty Eleven 1.0
  */
 
+// elastic slider
+$elasticSliderEnabled = TRUE;
+
 get_header(); ?>
 
 <?php
@@ -20,7 +23,92 @@ get_header(); ?>
 ?>
 
 		<div id="primary" class="sections-page">
-			<div id="content" role="main" class="custom-page-content">
+		<?php // elastic slider ?>
+		<div class="ei-slider-wrapper">
+                <div id="ei-slider" class="ei-slider">
+                    <ul class="ei-slider-large">
+                        <li>
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/large/lehrever.jpg" alt="image01" />
+                            <div class="ei-title">
+                                <h2>Lehrveranstaltungen</h2>
+                                <h3>der Dozenten des Instituts</h3>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/large/germanistikstudium.jpg" alt="image02" />
+                            <div class="ei-title">
+                                <h2>Germanistikstudium</h2>
+                                <h3>in Szeged</h3>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/large/interviews-mit-gastdoz.jpg" alt="image03"/>
+                            <div class="ei-title">
+                                <h2>Interviews</h2>
+                                <h3>mit Gastdozenten des Instituts</h3>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/large/germanisten-party.jpg" alt="image04"/>
+                            <div class="ei-title">
+                                <h2>Germanistenparty</h2>
+                                <h3>&nbsp;</h3>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/large/medien.jpg" alt="image05"/>
+                            <div class="ei-title">
+                                <h2>Medien</h2>
+                                <h3>Internet, Presse, Fernsehen, Rundfunk</h3>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/large/musik.jpg" alt="image06"/>
+                            <div class="ei-title">
+                                <h2>Musik</h2>
+                                <h3>&nbsp;</h3>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/large/praktikum.jpg" alt="image07"/>
+                            <div class="ei-title">
+                                <h2>Unterrichtspraktikum</h2>
+                                <h3>&nbsp;</h3>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/large/wohnheime.jpg" alt="image07"/>
+                            <div class="ei-title">
+                                <h2>Studentenwohnheime</h2>
+								<h3>in Szeged</h3>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/large/interviews-mit-doz.jpg" alt="image07"/>
+                            <div class="ei-title">
+                                <h2>Interviews</h2>
+								<h3>mit Dozenten des Instituts</h3>
+                            </div>
+                        </li>
+                    </ul><!-- ei-slider-large -->
+                    <ul class="ei-slider-thumbs">
+                        <li class="ei-slider-element">Current</li>
+                        <li><a>Lehrveranstaltungen der Dozenten des Instituts</a><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/thumbs/lehrever.jpg" alt="" /></li>
+                        <li><a>Germanistikstudium in Szeged</a><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/thumbs/germanistikstudium.jpg" alt="" /></li>
+                        <li><a>Interviews mit Gastdozenten des Instituts</a><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/thumbs/interviews-mit-gastdoz.jpg" alt="" /></li>
+                        <li><a>Germanistenparty</a><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/thumbs/germanisten-party.jpg" alt="" /></li>
+                        <li><a>Medien</a><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/thumbs/medien.jpg" alt="" /></li>
+                        <li><a>Musik</a><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/thumbs/musik.jpg" alt="" /></li>
+                        <li><a>Unterrichtspraktikum</a><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/thumbs/praktikum.jpg" alt="" /></li>
+                        <li><a>Studentenwohnheime in Szeged</a><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/thumbs/wohnheime.jpg" alt="" /></li>
+                        <li><a>Slide 9</a><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slider/thumbs/interviews-mit-doz.jpg" alt="" /></li>
+                    </ul><!-- ei-slider-thumbs -->
+                </div><!-- ei-slider -->
+            </div><!-- wrapper -->			
+			
+			<?php // elastic slider ?>
+		
+			<!-- div id="content" role="main" class="custom-page-content">
 					<?php foreach($sections as $category): ?>
 						<a href="<?php echo($home . "/rubrik/" . $category->slug); ?>">
 							<div class="rubrik-container">
