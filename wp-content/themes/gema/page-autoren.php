8b86a9c2b6b88bd3e4745108b487663da166e685
Ferenc Fejer <FerencFejer@gmail.com>
2013-10-02 22:18:09 +0200

    Authors' page - first step

--- wp-content/themes/gema/page-autoren.php
+++ wp-content/themes/gema/page-autoren.php
@@ -17,10 +17,40 @@ get_header(); ?>
 		<div id="primary">
 			<div id="content" role="main">
 				<ul>
-					<?php foreach($terms = get_terms("author") as $category): ?>
-						<li><?php printf("%s (%d)", $category->name, $category->count);?></li>
-					<?php endforeach; ?>
-				</ul>			
+					
+				<?php
+				$authors = $wpdb->get_col(
+				    "SELECT um2.user_id
+				    FROM {$wpdb->usermeta} um1
+				    LEFT JOIN {$wpdb->usermeta} um2
+				         ON um1.user_id = um2.user_id
+				    WHERE um1.meta_key = 'wp_user_level'
+				    AND um1.meta_value = '2'
+				    AND um2.meta_key = 'last_name'
+				    ORDER by um2.meta_value ASC"
+				);
+				
+					$users = get_users( array('role' => 'author', 'fields' => 'all_with_meta') );
+					
+					foreach ($authors as $author) {
+						
+						$user_info = get_userdata($author);
+						  $first_name = $user_info->user_firstname;
+					      $last_name = $user_info->user_lastname;
+					    
+						echo "$first_name $last_name";
+					}
+					
+					$posts = get_posts();
+					
+					foreach ($posts as $post)
+					{
+						echo "<pre>";
+						print_r(get_coauthors($post->ID));
+						echo "</pre>";
+					}
+					
+				?>
 			</div><!-- #content -->
 		</div><!-- #primary -->
 
