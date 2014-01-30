142608ff654d838c0ff9477f3a2c97e21b23cb26
Ferenc Fejer <FerencFejer@gmail.com>
2014-01-30 22:13:20 +0100

    New category adjustment

--- wp-content/themes/gema/header.php
+++ wp-content/themes/gema/header.php
@@ -46,7 +46,7 @@
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>?v=3.0" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>?v=4.0" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
